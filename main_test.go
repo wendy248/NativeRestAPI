5aa448ab273be8d1842c42c0c37c0c43ed8a4913
Wendy <wendywibisono21@gmail.com>
2022-03-31 00:57:27 +0700

    Testing POST function

--- main_test.go
+++ main_test.go
@@ -13,28 +13,31 @@ Catatan :
  main_test.go hanya melakukan Unit Test pada function POST dari main.go,
  sehingga proses pengujian fungsi lainnya dilakukan dengan menggunakan program Postman.
 
+=== RUN   TestPostHandle
+--- PASS: TestPostHandle (0.00s)
+PASS
+ok      Github/NativeRestAPI    0.021s
 */
 
 func TestPostHandle(t *testing.T) {
-	var jsonStr = []byte(`{"id":1,"name":"budi","age":5}`)
-	req, err := http.NewRequest("POST", "/student", bytes.NewBuffer(jsonStr))
+	var data = []byte(`{"id":1,"name":"budi","age":5}`)
+	req, err := http.NewRequest("POST", "/student", bytes.NewBuffer(data))
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	req.Header.Set("Content-Type", "application/json")
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(controller.PostHandle)
-	handler.ServeHTTP(rr, req)
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
+	res := httptest.NewRecorder()
+	handler := http.HandlerFunc(controller.PostHandle)
+	handler.ServeHTTP(res, req)
+
+	if httpStatus := res.Code; httpStatus != http.StatusOK {
+		t.Errorf("http.status code from handler: %v, the code supposed to be %v", httpStatus, http.StatusOK)
 	}
 
-	expected := `{"Message": "Success input data to table"}`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+	value := `{"Message": "Success input data to table"}`
+	if res.Body.String() != value {
+		t.Errorf("value from handler: %v, the value supposed to be %v", res.Body.String(), value)
 	}
 }
